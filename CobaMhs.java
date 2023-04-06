71136b6aa25d3e4c5c05a065d97b2ab762960fb4
belsandb <129395766+belsandb@users.noreply.github.com>
2023-04-06 23:52:32 +0700

    Add files via upload

---
+++ CobaMhs.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class CobaMhs {
+    public static void main(String[] args){
+        Mhs datamhs = new Mhs();
+        
+        datamhs.setNama ("Arya Widyanto");
+        System.out.print ("Nama Mhs : " + datamhs.getNama());
+    }
+}
---
+++ Mhs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Mhs {
+    //property..
+    private String nama; 
+    private float ipk;
+    
+    //behavior...
+    public void setNama (String nama){
+        this.nama = nama;
+    }
+    public void setIpk (int ipk) {
+        this.ipk = ipk;
+    }
+    public String getNama (){
+        return this.nama;
+    }
+}
